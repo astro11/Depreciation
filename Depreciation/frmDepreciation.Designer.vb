67a43a3c6a686f7ab416f2004f5ef726af42b54e
astro <aquila.strother@asu.edu>
2016-09-02 15:49:27 -0400

    Added files

---
+++ Depreciation/frmDepreciation.Designer.vb
@@ -0,0 +1,348 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmDepreciation
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.mnuFile = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuDisplay = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuClear = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuExit = New System.Windows.Forms.ToolStripMenuItem()
+        Me.picTrends = New System.Windows.Forms.PictureBox()
+        Me.lblCompanyName = New System.Windows.Forms.Label()
+        Me.lblSelectItem = New System.Windows.Forms.Label()
+        Me.lstInventoryId = New System.Windows.Forms.ListBox()
+        Me.grpDepreciationType = New System.Windows.Forms.GroupBox()
+        Me.radDoubleDeclining = New System.Windows.Forms.RadioButton()
+        Me.radStraightLine = New System.Windows.Forms.RadioButton()
+        Me.btnCalculateDepreciation = New System.Windows.Forms.Button()
+        Me.lblItem = New System.Windows.Forms.Label()
+        Me.lblQuantity = New System.Windows.Forms.Label()
+        Me.lstYear = New System.Windows.Forms.ListBox()
+        Me.lstPresentValue = New System.Windows.Forms.ListBox()
+        Me.lstYearDepreciation = New System.Windows.Forms.ListBox()
+        Me.lstTotalDepreciation = New System.Windows.Forms.ListBox()
+        Me.lblYear = New System.Windows.Forms.Label()
+        Me.lblPresentValue = New System.Windows.Forms.Label()
+        Me.lblYearDepreciation = New System.Windows.Forms.Label()
+        Me.lblTotalDepreciation = New System.Windows.Forms.Label()
+        Me.MenuStrip1.SuspendLayout()
+        CType(Me.picTrends, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.grpDepreciationType.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuFile})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(684, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'mnuFile
+        '
+        Me.mnuFile.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuDisplay, Me.mnuClear, Me.mnuExit})
+        Me.mnuFile.Name = "mnuFile"
+        Me.mnuFile.Size = New System.Drawing.Size(37, 20)
+        Me.mnuFile.Text = "&File"
+        '
+        'mnuDisplay
+        '
+        Me.mnuDisplay.Name = "mnuDisplay"
+        Me.mnuDisplay.Size = New System.Drawing.Size(165, 22)
+        Me.mnuDisplay.Text = "&Display Inventory"
+        '
+        'mnuClear
+        '
+        Me.mnuClear.Name = "mnuClear"
+        Me.mnuClear.Size = New System.Drawing.Size(165, 22)
+        Me.mnuClear.Text = "&Clear"
+        '
+        'mnuExit
+        '
+        Me.mnuExit.Name = "mnuExit"
+        Me.mnuExit.Size = New System.Drawing.Size(165, 22)
+        Me.mnuExit.Text = "E&xit"
+        '
+        'picTrends
+        '
+        Me.picTrends.Image = Global.Depreciation.My.Resources.Resources.Logo
+        Me.picTrends.Location = New System.Drawing.Point(1, 25)
+        Me.picTrends.Name = "picTrends"
+        Me.picTrends.Size = New System.Drawing.Size(172, 223)
+        Me.picTrends.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picTrends.TabIndex = 1
+        Me.picTrends.TabStop = False
+        '
+        'lblCompanyName
+        '
+        Me.lblCompanyName.AutoSize = True
+        Me.lblCompanyName.Font = New System.Drawing.Font("Impact", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCompanyName.ForeColor = System.Drawing.Color.DarkBlue
+        Me.lblCompanyName.Location = New System.Drawing.Point(224, 27)
+        Me.lblCompanyName.Name = "lblCompanyName"
+        Me.lblCompanyName.Size = New System.Drawing.Size(406, 39)
+        Me.lblCompanyName.TabIndex = 2
+        Me.lblCompanyName.Text = "Trends Menswear Inventory"
+        '
+        'lblSelectItem
+        '
+        Me.lblSelectItem.AutoSize = True
+        Me.lblSelectItem.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSelectItem.ForeColor = System.Drawing.Color.Gray
+        Me.lblSelectItem.Location = New System.Drawing.Point(231, 85)
+        Me.lblSelectItem.Name = "lblSelectItem"
+        Me.lblSelectItem.Size = New System.Drawing.Size(156, 16)
+        Me.lblSelectItem.TabIndex = 3
+        Me.lblSelectItem.Text = "Select Inventory Item:"
+        '
+        'lstInventoryId
+        '
+        Me.lstInventoryId.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.lstInventoryId.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstInventoryId.ForeColor = System.Drawing.Color.Gray
+        Me.lstInventoryId.FormattingEnabled = True
+        Me.lstInventoryId.ItemHeight = 16
+        Me.lstInventoryId.Location = New System.Drawing.Point(234, 105)
+        Me.lstInventoryId.Name = "lstInventoryId"
+        Me.lstInventoryId.Size = New System.Drawing.Size(153, 84)
+        Me.lstInventoryId.TabIndex = 4
+        '
+        'grpDepreciationType
+        '
+        Me.grpDepreciationType.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.grpDepreciationType.Controls.Add(Me.radDoubleDeclining)
+        Me.grpDepreciationType.Controls.Add(Me.radStraightLine)
+        Me.grpDepreciationType.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.grpDepreciationType.ForeColor = System.Drawing.Color.Gray
+        Me.grpDepreciationType.Location = New System.Drawing.Point(407, 85)
+        Me.grpDepreciationType.Name = "grpDepreciationType"
+        Me.grpDepreciationType.Size = New System.Drawing.Size(223, 100)
+        Me.grpDepreciationType.TabIndex = 5
+        Me.grpDepreciationType.TabStop = False
+        Me.grpDepreciationType.Text = "Select Depreciation Method:"
+        '
+        'radDoubleDeclining
+        '
+        Me.radDoubleDeclining.AutoSize = True
+        Me.radDoubleDeclining.Location = New System.Drawing.Point(7, 60)
+        Me.radDoubleDeclining.Name = "radDoubleDeclining"
+        Me.radDoubleDeclining.Size = New System.Drawing.Size(207, 20)
+        Me.radDoubleDeclining.TabIndex = 1
+        Me.radDoubleDeclining.TabStop = True
+        Me.radDoubleDeclining.Text = "Double-Declining Balance"
+        Me.radDoubleDeclining.UseVisualStyleBackColor = True
+        '
+        'radStraightLine
+        '
+        Me.radStraightLine.AutoSize = True
+        Me.radStraightLine.Location = New System.Drawing.Point(7, 30)
+        Me.radStraightLine.Name = "radStraightLine"
+        Me.radStraightLine.Size = New System.Drawing.Size(113, 20)
+        Me.radStraightLine.TabIndex = 0
+        Me.radStraightLine.TabStop = True
+        Me.radStraightLine.Text = "Straight-Line"
+        Me.radStraightLine.UseVisualStyleBackColor = True
+        '
+        'btnCalculateDepreciation
+        '
+        Me.btnCalculateDepreciation.BackColor = System.Drawing.Color.DarkBlue
+        Me.btnCalculateDepreciation.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCalculateDepreciation.ForeColor = System.Drawing.Color.White
+        Me.btnCalculateDepreciation.Location = New System.Drawing.Point(304, 204)
+        Me.btnCalculateDepreciation.Name = "btnCalculateDepreciation"
+        Me.btnCalculateDepreciation.Size = New System.Drawing.Size(198, 35)
+        Me.btnCalculateDepreciation.TabIndex = 6
+        Me.btnCalculateDepreciation.Text = "Calculate Depreciation"
+        Me.btnCalculateDepreciation.UseVisualStyleBackColor = False
+        '
+        'lblItem
+        '
+        Me.lblItem.AutoSize = True
+        Me.lblItem.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblItem.Location = New System.Drawing.Point(269, 253)
+        Me.lblItem.Name = "lblItem"
+        Me.lblItem.Size = New System.Drawing.Size(278, 16)
+        Me.lblItem.TabIndex = 7
+        Me.lblItem.Text = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+        Me.lblItem.Visible = False
+        '
+        'lblQuantity
+        '
+        Me.lblQuantity.AutoSize = True
+        Me.lblQuantity.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblQuantity.Location = New System.Drawing.Point(361, 269)
+        Me.lblQuantity.Name = "lblQuantity"
+        Me.lblQuantity.Size = New System.Drawing.Size(89, 16)
+        Me.lblQuantity.TabIndex = 8
+        Me.lblQuantity.Text = "XXXXXXXXX"
+        Me.lblQuantity.Visible = False
+        '
+        'lstYear
+        '
+        Me.lstYear.ForeColor = System.Drawing.Color.Gray
+        Me.lstYear.FormattingEnabled = True
+        Me.lstYear.Items.AddRange(New Object() {"1", "2", "3", "4", "5"})
+        Me.lstYear.Location = New System.Drawing.Point(125, 321)
+        Me.lstYear.Name = "lstYear"
+        Me.lstYear.Size = New System.Drawing.Size(88, 82)
+        Me.lstYear.TabIndex = 9
+        Me.lstYear.Visible = False
+        '
+        'lstPresentValue
+        '
+        Me.lstPresentValue.ForeColor = System.Drawing.Color.Gray
+        Me.lstPresentValue.FormattingEnabled = True
+        Me.lstPresentValue.Items.AddRange(New Object() {"$345.00", "$276.00", "$207.00", "$138.00", "$69.00"})
+        Me.lstPresentValue.Location = New System.Drawing.Point(268, 321)
+        Me.lstPresentValue.Name = "lstPresentValue"
+        Me.lstPresentValue.Size = New System.Drawing.Size(78, 82)
+        Me.lstPresentValue.TabIndex = 10
+        Me.lstPresentValue.Visible = False
+        '
+        'lstYearDepreciation
+        '
+        Me.lstYearDepreciation.ForeColor = System.Drawing.Color.Gray
+        Me.lstYearDepreciation.FormattingEnabled = True
+        Me.lstYearDepreciation.Items.AddRange(New Object() {"$69.00", "$69.00", "$69.00", "$69.00", "$69.00"})
+        Me.lstYearDepreciation.Location = New System.Drawing.Point(401, 321)
+        Me.lstYearDepreciation.Name = "lstYearDepreciation"
+        Me.lstYearDepreciation.Size = New System.Drawing.Size(78, 82)
+        Me.lstYearDepreciation.TabIndex = 11
+        Me.lstYearDepreciation.Visible = False
+        '
+        'lstTotalDepreciation
+        '
+        Me.lstTotalDepreciation.ForeColor = System.Drawing.Color.Gray
+        Me.lstTotalDepreciation.FormattingEnabled = True
+        Me.lstTotalDepreciation.Items.AddRange(New Object() {"$69.00", "$138.00", "$207.00", "$276.00", "$345.00"})
+        Me.lstTotalDepreciation.Location = New System.Drawing.Point(534, 321)
+        Me.lstTotalDepreciation.Name = "lstTotalDepreciation"
+        Me.lstTotalDepreciation.Size = New System.Drawing.Size(78, 82)
+        Me.lstTotalDepreciation.TabIndex = 12
+        Me.lstTotalDepreciation.Visible = False
+        '
+        'lblYear
+        '
+        Me.lblYear.AutoSize = True
+        Me.lblYear.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblYear.Location = New System.Drawing.Point(125, 288)
+        Me.lblYear.Name = "lblYear"
+        Me.lblYear.Size = New System.Drawing.Size(61, 32)
+        Me.lblYear.TabIndex = 13
+        Me.lblYear.Text = "Present" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Year"
+        Me.lblYear.Visible = False
+        '
+        'lblPresentValue
+        '
+        Me.lblPresentValue.AutoSize = True
+        Me.lblPresentValue.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPresentValue.Location = New System.Drawing.Point(268, 288)
+        Me.lblPresentValue.Name = "lblPresentValue"
+        Me.lblPresentValue.Size = New System.Drawing.Size(65, 32)
+        Me.lblPresentValue.TabIndex = 14
+        Me.lblPresentValue.Text = "Present " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Value"
+        Me.lblPresentValue.Visible = False
+        '
+        'lblYearDepreciation
+        '
+        Me.lblYearDepreciation.AutoSize = True
+        Me.lblYearDepreciation.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblYearDepreciation.Location = New System.Drawing.Point(398, 288)
+        Me.lblYearDepreciation.Name = "lblYearDepreciation"
+        Me.lblYearDepreciation.Size = New System.Drawing.Size(97, 32)
+        Me.lblYearDepreciation.TabIndex = 15
+        Me.lblYearDepreciation.Text = "Year " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Depreciation"
+        Me.lblYearDepreciation.Visible = False
+        '
+        'lblTotalDepreciation
+        '
+        Me.lblTotalDepreciation.AutoSize = True
+        Me.lblTotalDepreciation.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTotalDepreciation.Location = New System.Drawing.Point(531, 288)
+        Me.lblTotalDepreciation.Name = "lblTotalDepreciation"
+        Me.lblTotalDepreciation.Size = New System.Drawing.Size(97, 32)
+        Me.lblTotalDepreciation.TabIndex = 16
+        Me.lblTotalDepreciation.Text = "Total " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Depreciation"
+        Me.lblTotalDepreciation.Visible = False
+        '
+        'frmDepreciation
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(684, 431)
+        Me.Controls.Add(Me.lblTotalDepreciation)
+        Me.Controls.Add(Me.lblYearDepreciation)
+        Me.Controls.Add(Me.lblPresentValue)
+        Me.Controls.Add(Me.lblYear)
+        Me.Controls.Add(Me.lstTotalDepreciation)
+        Me.Controls.Add(Me.lstYearDepreciation)
+        Me.Controls.Add(Me.lstPresentValue)
+        Me.Controls.Add(Me.lstYear)
+        Me.Controls.Add(Me.lblQuantity)
+        Me.Controls.Add(Me.lblItem)
+        Me.Controls.Add(Me.btnCalculateDepreciation)
+        Me.Controls.Add(Me.grpDepreciationType)
+        Me.Controls.Add(Me.lstInventoryId)
+        Me.Controls.Add(Me.lblSelectItem)
+        Me.Controls.Add(Me.lblCompanyName)
+        Me.Controls.Add(Me.picTrends)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.ForeColor = System.Drawing.Color.Gray
+        Me.Name = "frmDepreciation"
+        Me.Text = "Compute Depreciation by Method"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        CType(Me.picTrends, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.grpDepreciationType.ResumeLayout(False)
+        Me.grpDepreciationType.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents mnuFile As ToolStripMenuItem
+    Friend WithEvents mnuDisplay As ToolStripMenuItem
+    Friend WithEvents mnuClear As ToolStripMenuItem
+    Friend WithEvents mnuExit As ToolStripMenuItem
+    Friend WithEvents picTrends As PictureBox
+    Friend WithEvents lblCompanyName As Label
+    Friend WithEvents lblSelectItem As Label
+    Friend WithEvents lstInventoryId As ListBox
+    Friend WithEvents grpDepreciationType As GroupBox
+    Friend WithEvents radDoubleDeclining As RadioButton
+    Friend WithEvents radStraightLine As RadioButton
+    Friend WithEvents btnCalculateDepreciation As Button
+    Friend WithEvents lblItem As Label
+    Friend WithEvents lblQuantity As Label
+    Friend WithEvents lstYear As ListBox
+    Friend WithEvents lstPresentValue As ListBox
+    Friend WithEvents lstYearDepreciation As ListBox
+    Friend WithEvents lstTotalDepreciation As ListBox
+    Friend WithEvents lblYear As Label
+    Friend WithEvents lblPresentValue As Label
+    Friend WithEvents lblYearDepreciation As Label
+    Friend WithEvents lblTotalDepreciation As Label
+End Class
---
+++ Depreciation/frmDisplayInventory.Designer.vb
@@ -0,0 +1,101 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmDisplayInventory
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.picMan2 = New System.Windows.Forms.PictureBox()
+        Me.lblSortedInventory = New System.Windows.Forms.Label()
+        Me.lstDisplay = New System.Windows.Forms.ListBox()
+        Me.btnReturn = New System.Windows.Forms.Button()
+        CType(Me.picMan2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'picMan2
+        '
+        Me.picMan2.Image = Global.Depreciation.My.Resources.Resources.Logo
+        Me.picMan2.Location = New System.Drawing.Point(0, 12)
+        Me.picMan2.Name = "picMan2"
+        Me.picMan2.Size = New System.Drawing.Size(109, 90)
+        Me.picMan2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picMan2.TabIndex = 0
+        Me.picMan2.TabStop = False
+        '
+        'lblSortedInventory
+        '
+        Me.lblSortedInventory.AutoSize = True
+        Me.lblSortedInventory.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSortedInventory.ForeColor = System.Drawing.Color.DarkBlue
+        Me.lblSortedInventory.Location = New System.Drawing.Point(115, 33)
+        Me.lblSortedInventory.Name = "lblSortedInventory"
+        Me.lblSortedInventory.Size = New System.Drawing.Size(278, 25)
+        Me.lblSortedInventory.TabIndex = 1
+        Me.lblSortedInventory.Text = "Trends Menswear Inventory"
+        '
+        'lstDisplay
+        '
+        Me.lstDisplay.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.lstDisplay.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstDisplay.ForeColor = System.Drawing.Color.Gray
+        Me.lstDisplay.FormattingEnabled = True
+        Me.lstDisplay.ItemHeight = 20
+        Me.lstDisplay.Items.AddRange(New Object() {"Chairs", "End Cap Displays", "Hangers", "Mannequins", "Office Computers", "Point of Sale Registers", "Sofas", "Store Display Racks"})
+        Me.lstDisplay.Location = New System.Drawing.Point(120, 94)
+        Me.lstDisplay.Name = "lstDisplay"
+        Me.lstDisplay.Size = New System.Drawing.Size(207, 164)
+        Me.lstDisplay.TabIndex = 2
+        '
+        'btnReturn
+        '
+        Me.btnReturn.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.btnReturn.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnReturn.ForeColor = System.Drawing.Color.Gray
+        Me.btnReturn.Location = New System.Drawing.Point(120, 301)
+        Me.btnReturn.Name = "btnReturn"
+        Me.btnReturn.Size = New System.Drawing.Size(207, 49)
+        Me.btnReturn.TabIndex = 3
+        Me.btnReturn.Text = "Return to Application"
+        Me.btnReturn.UseVisualStyleBackColor = False
+        '
+        'frmDisplayInventory
+        '
+        Me.AcceptButton = Me.btnReturn
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(405, 381)
+        Me.Controls.Add(Me.btnReturn)
+        Me.Controls.Add(Me.lstDisplay)
+        Me.Controls.Add(Me.lblSortedInventory)
+        Me.Controls.Add(Me.picMan2)
+        Me.Name = "frmDisplayInventory"
+        Me.Text = "Sorted Inventory Listing"
+        CType(Me.picMan2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents picMan2 As PictureBox
+    Friend WithEvents lblSortedInventory As Label
+    Friend WithEvents lstDisplay As ListBox
+    Friend WithEvents btnReturn As Button
+End Class
